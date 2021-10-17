7ea889cb5a6321069cecec6201ac62a6ad844a97
Petinho29 <petkovic.paiva@hotmail.com>
2021-10-17 07:26:06 -0300

    Merchant // Merlin - Update

---
+++ Dungeon/Assets/cods/BulletScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : EnemyHitbox
+{
+    public GameObject hitEffect;
+
+    [SerializeField]
+    float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D Col)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 1f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Dungeon/Assets/cods/PlayerMovement2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : Player
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Os float's abaixo são parametros para a animação, dessa forma é possivel criar o Blend Tree
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
