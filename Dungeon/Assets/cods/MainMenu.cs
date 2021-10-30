1e81f193ef8a2c17394fd7c5949b843a77691a0b
Matheus <matheusfsantos.152@gmail.com>
2021-10-30 17:29:13 -0300

    MainMenu
    Matheus: add main menu funcional.
    OBS: muitos arquivos por causa da extensão TextMeshPro

---
+++ Dungeon/Assets/cods/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("Testes");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
