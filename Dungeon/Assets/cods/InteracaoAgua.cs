892c05d86e477527afaf65089fbbb59814210ae4
Petinho29 <petkovic.paiva@hotmail.com>
2021-11-24 12:08:24 -0300

    Ajustes
    Petkovic: Adição de alguns componentes no cenário do passado

---
+++ Dungeon/Assets/cods/InteracaoAgua.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteracaoAgua : Collidable
+{
+    Animator animator;
+    private int health = 2;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Bullet")
+        {
+            Destroy(coll.gameObject);
+            health--;
+
+
+            if (health == 1)
+            {
+                animator.Play("AguaGelo");
+                FindObjectOfType<AudioManager>().Play("IceSound");
+
+            }
+
+        }
+    }
+}
